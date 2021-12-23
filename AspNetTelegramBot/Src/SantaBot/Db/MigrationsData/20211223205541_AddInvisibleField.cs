1d23f554ac9f698c1356d958a7a8dacd7337c5ab
Burtimax <timofeyburkush@gmail.com>
2021-12-24 01:22:41 +0300

    миграции

---
+++ AspNetTelegramBot/Src/SantaBot/Db/MigrationsData/20211223205541_AddInvisibleField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MarathonBot.Src.SantaBot.Db.MigrationsData
+{
+    public partial class AddInvisibleField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Invisible",
+                schema: "santa",
+                table: "UsersInfo",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Invisible",
+                schema: "santa",
+                table: "UsersInfo");
+        }
+    }
+}
